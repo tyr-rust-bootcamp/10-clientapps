9f6fb2c6374f602595f6a6f41a87f976bd5db800
Tyr Chen <tyr.chen@gmail.com>
2024-06-09 14:16:58 -0700

    chore: init project

---
+++ hackernews/src/main.rs
@@ -0,0 +1,65 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use tracing::Level;
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::Home {}, "Go to counter" }
+        div { class: "flex flex-col items-center justify-center h-screen",
+            h1 { class: "text-3xl p-4", "Blog post {id}" }
+        }
+    }
+}
+
+#[component]
+fn NotFound() -> Element {
+    rsx! {
+        div { class: "flex flex-col items-center justify-center h-screen",
+            h1 { class: "text-3xl p-4", "404 Not Found" }
+        }
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    let mut count = use_signal(|| 0);
+
+    rsx! {
+        Link { to: Route::Blog { id: count() }, "Go to blog" }
+        div { class: "flex flex-col items-center justify-center h-screen",
+            h1 { class: "text-3xl p-4", "High-Five counter: {count}" }
+            button {
+                class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
+                onclick: move |_| count += 1,
+                "Up high!"
+            }
+            button {
+                class: "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
+                onclick: move |_| count -= 1,
+                "Down low!"
+            }
+        }
+    }
+}
